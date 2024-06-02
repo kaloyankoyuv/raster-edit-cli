868944a22fb30da852e39d85a3b5eb6ed72363e5
Kaloyan Koyuv <kaloyankoyuv@gmail.com>
2024-06-02 18:48:40 +0300

    adding help command

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,49 @@
 #include "session/session.h"
 #include <iostream>
 
+void help() {
+  std::cout
+      << "The following commands are supported:\n"
+      << "help - Displays this help message\n"
+      << "exit - Kills all sessions and exits the program\n"
+      << "load <file> - Creates a new session and adds <file> to it\n"
+      << "switch <session id> - Switches to session with id <session "
+         "id>\n"
+      << "session info - Displays info about the current session\n"
+      << "add <file> - Adds <file> to the current session\n"
+      << "remove <file> - Removes <file> from the current sessoin\n"
+      << "undo - Removes last pending operation from the current session\n"
+      << "apply - Applies all pending operations in the current session\n"
+      << "save - Saves all the files in the current session\n"
+      << "saveas <file> - Saves the first loaded image in the current "
+         "session as <file>\n"
+      << "monochrome - Turns all images in the current session monochrome\n"
+      << "grayscale - Turns all images in the current session grayscale\n"
+      << "negative - Turns all images in the current session negative\n"
+      << "rotate <left/right> - Rotates all images in the current session "
+         "left/right\n"
+      << "collage <horizontal/vertical> <file1> <file2> <outfile> - Makes "
+         "a horizontal/vertical collage from <file1> and <file2> and saves "
+         "it to <outfile>. The collage is then added to the current "
+         "session\n"
+      << "scale <factor> <file> <outfile> - Makes a new scaled by factor "
+         "of <factor> image and saves it to <outfile>. The scaled image is "
+         "then added to the current session\n";
+}
+
 int main() {
 
   int id_num = -1;
   int curr_session_id = id_num;
   std::vector<Session *> sessions;
   std::string input;
+  std::cout << "Welcome to raster-edit-cli!\n"
+            << "Use command \"help\" to view all commands\n";
   while (true) {
     std::cout << "Enter command > ";
     std::cin >> input;
-    if ((input != "load" && input != "help") && id_num == -1) {
+    if ((input != "load" && input != "help" && input != "exit") &&
+        id_num == -1) {
       std::cout << "No loaded session\n";
       continue;
     }
@@ -55,7 +88,7 @@ int main() {
       std::cout << "Exiting!\n";
       break;
     } else if (input == "help") {
-      std::cout << "RTFM\n";
+      help();
     } else if (input == "apply") {
       sessions[curr_session_id]->apply();
     } else if (input == "save") {
@@ -77,22 +110,13 @@ int main() {
         sessions[curr_session_id]->add_operation("rotate right");
       }
     } else if (input == "collage") {
-      std::string direction;
-      std::cin >> direction;
-      std::string img1;
-      std::cin >> img1;
-      std::string img2;
-      std::cin >> img2;
-      std::string outimage;
-      std::cin >> outimage;
+      std::string direction, img1, img2, outimage;
+      std::cin >> direction >> img1 >> img2 >> outimage;
       sessions[curr_session_id]->collage(direction, img1, img2, outimage);
     } else if (input == "scale") {
       int factor;
-      std::cin >> factor;
-      std::string img;
-      std::cin >> img;
-      std::string out;
-      std::cin >> out;
+      std::string img, out;
+      std::cin >> factor >> img >> out;
       sessions[curr_session_id]->scale(factor, img, out);
     }
   }
